5079dd73b8d1755e30fcefc16471e1838b5ab6dd
abdelchakour-svj <abdelchakour2@gmail.com>
2025-12-09 13:49:35 +0100

    Update avl.c

--- avl.c
+++ avl.c
@@ -1,1 +1,16 @@
 
+AVL* creerAVL(int e)
+{
+    // Alloue de la mémoire pour un nouveau nœud
+    AVL* new = (AVL* )malloc(sizeof(AVL));
+    if (new == NULL)
+    {
+        exit(EXIT_FAILURE); // Arrêt immédiat en cas d'erreur d'allocation
+    }
+    new->value = e; // Initialisation de la valeur
+    new->fg = NULL; // Pas de fils gauche
+    new->fd = NULL; // Pas de fils droit
+    new->eq = 0;    // Facteur d'équilibre initialisé à 0
+    return new;
+}
+
