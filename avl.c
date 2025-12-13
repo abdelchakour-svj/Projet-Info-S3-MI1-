359b82317b00c7d5e959f50b7d15d500c02cce1d
talal95c <talalaamir958@gmail.com>
2025-12-13 16:25:06 +0100

    Update avl.c

--- avl.c
+++ avl.c
@@ -8,19 +8,21 @@ typedef struct AVL_struct{
 // ajt id usine 
 }AVL;
 
-
-
-AVL* creationAVL(int e)
-{
-    AVL* nouveau = (AVL* )malloc(sizeof(AVL));
-    if (nouveau == NULL)
-    {
-        exit(EXIT_FAILURE); 
+AVLNode* creer_noeud_avl(const char *identifiant) {
+    AVLNode *noeud = (AVLNode*)malloc(sizeof(AVLNode));
+    if (noeud == NULL) {
+        fprintf(stderr, "Erreur allocation mémoire\n");
+        return NULL;
     }
-    nouveau->value = e; 
-    nouveau->fg = NULL; 
-    nouveau->fd = NULL; 
-    nouveau->eq = 0;    
-    return nouveau;
-}
+    
+    noeud->identifiant = strdup(identifiant);
+    noeud->capacite_max = 0.0;
+    noeud->volume_capte = 0.0;
+    noeud->volume_traite = 0.0;
+    noeud->gauche = NULL;
+    noeud->droite = NULL;
+    noeud->hauteur = 1;
+    
+    return noeud;
+}}
 
